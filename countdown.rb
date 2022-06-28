92d386e990e495e432b009ebd54306dfdb5c73fc
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-06-27 23:28:10 -0700

    commit tree 40a00c496edfc8ef412d7f84c1c2a682677c9b5a

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(to_midnight)
+    while to_midnight > 0
+        puts "#{to_midnight} SECOND(S)!"
+    to_midnight -= 1
+    
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_midnight)
+  sleep(5.seconds)  
+    seconds_midnight -= 1
+    end
+        "HAPPY NEW YEAR!"
 end
